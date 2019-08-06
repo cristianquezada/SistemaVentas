e9f24758bc03e9fb0e4368dd0041f4f85bbd23bd
cristian <crisquezada38@gmail.com>
2019-08-06 17:53:57 -0400

    se crearon la tabla categoria y productos con algunos campos básicos

---
+++ database/migrations/2019_08_06_213025_add_productos_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->Increments('idProducto');
+            $table->string('nomProducto',100);
+            $table->string('codProducto',100);
+            $table->integer('precioCosto');
+            $table->integer('precioVenta');
+            $table->integer('stockProducto');
+            $table->string('descripcionProducto',200);
+
+            
+            $table->integer('idCategoria')->unsigned();
+            $table->foreign('idCategoria')->references('idCategoria')->on('categorias')->onDelete('cascade');
+
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
